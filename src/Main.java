33b60c6204999d2deb0215feab8f2f8fb017eb8e
thanujadasari <thanujadasari02@gmail.com>
2026-02-18 14:09:22 +0530

    First commit of the day

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome checker management system");
+        System.out.println("Version: 1.0");
+        System.out.println("System initiated successfully");
+
+    }
+}
